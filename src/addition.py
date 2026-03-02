5f3f8b58d8a5ec4152e1b72bdf29afb05b3624f8
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2026-03-02 18:01:35 +0530

    Update add function to include additional 100
    Modified the add function to include an additional 100 for testing purposes.

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,7 @@
 # This is a test commit
 # new commit
 def add(a, b):
-    return a + b
+    return a + b + 100 # to test personal alert
     # run the below code to test slack email notification on pipeline failure
     # return a + b + 100
 
